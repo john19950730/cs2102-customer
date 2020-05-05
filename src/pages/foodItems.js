b974fb1df52884616c2dd28742edf7fe134674cf
NewNoob <john19950730@gmail.com>
2020-05-05 08:27:21 +0800

    Prepare food items page

---
+++ src/pages/foodItems.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/foodItems.css';
+
+const FoodItems = () => (
+	<div className="fooditems">
+		<div className="fooditems-label">
+			<span className="step-number">2</span>FOOD ITEMS
+		</div>
+	</div>
+);
+
+export default FoodItems;
